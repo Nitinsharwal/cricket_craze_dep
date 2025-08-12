5f1af26703ec7c796010d3085c73f2bc1b1d9745
Nitin <nitinparjapat94@gail.com>
2025-08-12 17:07:16 +0530

    Initial commit

--- crex_prj/settings.py
+++ crex_prj/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'crex_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',  
+        'USER': 'root',     
+        'PASSWORD': 'cWCcYqggeVflSyQtgASWHnGLxMnhEVKE', 
+        'HOST': 'crossover.proxy.rlwy.net',
+        'PORT': '40783',                 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
